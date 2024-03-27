6b6b469dd54d35a172c313468ab6827a6ea50885
Mon613 <nguyenha61314@gmail.com>
2024-03-27 15:50:44 +0700

    update homepage

---
+++ app/src/main/java/com/example/musicappplayer/fragment/FragmentHome.java
@@ -0,0 +1,23 @@
+package com.example.musicappplayer.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.musicappplayer.R;
+
+public class FragmentHome extends Fragment {
+    View view;
+    @Nullable
+    @androidx.annotation.Nullable
+    @Override
+    public View onCreateView(@NonNull @androidx.annotation.NonNull LayoutInflater inflater, @Nullable @androidx.annotation.Nullable ViewGroup container, @Nullable @androidx.annotation.Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/musicappplayer/fragment/FragmentSearch.java
@@ -0,0 +1,24 @@
+package com.example.musicappplayer.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.musicappplayer.R;
+
+public class FragmentSearch extends Fragment {
+    View view;
+
+    @Nullable
+    @androidx.annotation.Nullable
+    @Override
+    public View onCreateView(@NonNull @androidx.annotation.NonNull LayoutInflater inflater, @Nullable @androidx.annotation.Nullable ViewGroup container, @Nullable @androidx.annotation.Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_search,container,false);
+        return view;
+    }
+}
