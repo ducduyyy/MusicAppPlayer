5d3276e7dfd4c2e6773667c854b4a855dbfc63dd
Mon613 <nguyenha61314@gmail.com>
2024-03-30 15:06:13 +0700

    update playlist

---
+++ app/src/main/java/com/example/musicappplayer/model/Playlist.java
@@ -0,0 +1,57 @@
+package com.example.musicappplayer.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import javax.annotation.processing.Generated;
+
+@Generated("jsonschema2pojo")
+public class Playlist {
+
+    @SerializedName("IdPlayList")
+    @Expose
+    private String idPlayList;
+    @SerializedName("Ten")
+    @Expose
+    private String ten;
+    @SerializedName("HinhNen")
+    @Expose
+    private String hinhNen;
+    @SerializedName("HinhIcon")
+    @Expose
+    private String hinhIcon;
+
+    public String getIdPlayList() {
+        return idPlayList;
+    }
+
+    public void setIdPlayList(String idPlayList) {
+        this.idPlayList = idPlayList;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getHinhNen() {
+        return hinhNen;
+    }
+
+    public void setHinhNen(String hinhNen) {
+        this.hinhNen = hinhNen;
+    }
+
+    public String getHinhIcon() {
+        return hinhIcon;
+    }
+
+    public void setHinhIcon(String hinhIcon) {
+        this.hinhIcon = hinhIcon;
+    }
+
+}
--- app/src/main/java/com/example/musicappplayer/service/Dataservice.java
+++ app/src/main/java/com/example/musicappplayer/service/Dataservice.java
@@ -1,6 +1,7 @@
 package com.example.musicappplayer.service;
 
 import com.example.musicappplayer.model.Banner;
+import com.example.musicappplayer.model.Playlist;
 
 import java.util.List;
 
@@ -10,4 +11,6 @@ import retrofit2.http.GET;
 public interface Dataservice {
     @GET("songbanner.php")
     Call<List<Banner>> getDataBanner();
+    @GET("playlistforcurrentday.php")
+    Call<List<Playlist>> getPlaylistCurrenDay();
 }
